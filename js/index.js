8338c4cb7ba953579b269d487da1765baa34ef93
VladMihalache <59883066+VladMihalache@users.noreply.github.com>
2020-07-13 20:25:43 +0300

    Note!

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-const input1= document.getElementById("theInput1");
+const input= document.getElementById("theInput");
 const button= document.querySelector(".button");
+const invisibleMessage = document.querySelector(".invisibleMessage")
 const timez= document.querySelector(".timezone");
 const tempC= document.querySelector(".tempC");
 const tempF= document.querySelector(".tempF");
@@ -10,28 +11,32 @@ const wicon= document.querySelector(".w-icon");
 const unit= document.querySelector(".unit");
 
 
-let apiKey="a8da44b6c3bc48d19c1a1b2e868a503b"
+
+button.addEventListener("click",() =>{
+    let city = input.value;
+    const urlToFetch= `https://api.weatherbit.io/v2.0/current?city=${city}&key=7bd157fd1c9842b5ab673580ec437d05`
+    if(city){
+        invisibleMessage.style.display = "none";
+        fetch(urlToFetch)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                const {app_temp,rh,weather,timezone} = data.data[0]
+                timez.innerHTML=timezone;
+                tempC.innerHTML= app_temp + " °C";
+                app_temp_F=app_temp * 1.8 + 32
+                tempF.innerHTML=app_temp_F + " F";
+                humid.innerHTML=rh + "%";
+                wdescrip.innerHTML=weather.description;
+            })
+    } else {
+        invisibleMessage.style.display = "flex";
+    }
+})
 
 toggleButton.addEventListener("change",() => {
     tempF.classList.toggle("displayNone")
     tempC.classList.toggle("displayNone")
-})
-
-
-button.addEventListener("click",() =>{
-    let city=input1.value;
-    const urlToFetch= `https://api.weatherbit.io/v2.0/current?city=${city}&key=a8da44b6c3bc48d19c1a1b2e868a503b`
-    fetch(urlToFetch)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            const {app_temp,rh,weather,timezone} = data.data[0]
-            timez.innerHTML=timezone;
-            tempC.innerHTML= app_temp + " °C";
-            app_temp_F=app_temp * 1.8 + 32
-            tempF.innerHTML=app_temp_F + " F";
-            humid.innerHTML=rh + "%";
-            wdescrip.innerHTML=weather.description;
-        })
 })
